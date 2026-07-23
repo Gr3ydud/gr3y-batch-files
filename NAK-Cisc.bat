7b6fc49bfa9f5ec0473778ffad3c401b25bae497
Gr3ydud <509hamster@gmail.com>
2026-07-22 22:02:36 -0700

    Update NAK-Cisc.bat

--- NAK-Cisc.bat
+++ NAK-Cisc.bat
@@ -4,21 +4,22 @@ cd "%userprofile%\desktop"
 color 4
 cls
 echo info
-echo ===============================
+echo ================================
 echo NAK-88 "Cisc" Edition
 echo "TBTP"
-echo VERSION 2 "Cerium"
+echo VERSION UNSTABLE-BETA-2 "Cerium"
 ipconfig | findstr /i "IPv4"
-echo ===============================
+echo ================================
 echo Inputs
-echo ===============================
+color 9
+echo ================================
 echo  N to launch ?
 echo  Z to launch ?
 echo  X to return
 echo  V to renew ip
 echo  C to run a python thing, tbd
 echo  1 to exit
-echo ===============================
+echo ================================
 echo Command-Line Interface
 set /p choice="NAK-88 Input>> "
 if /i "%choice%"=="A" goto LaunchA
