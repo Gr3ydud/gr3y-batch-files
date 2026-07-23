4fe670fb0c85516273ec696f6a332d2f4542b759
Gr3ydud <509hamster@gmail.com>
2026-07-23 07:57:29 -0700

    Update NAK-Cisc.bat

--- NAK-Cisc.bat
+++ NAK-Cisc.bat
@@ -4,29 +4,30 @@ cd "%userprofile%\desktop"
 color 4
 cls
 echo info
-echo ================================
+echo =================================
 echo NAK-88 "Cisc" Edition
 echo "TBTP"
 echo VERSION UNSTABLE-BETA-2 "Cerium"
 ipconfig | findstr /i "IPv4"
-echo ================================
+echo =================================
 echo Inputs
 color 9
-echo ================================
+echo =================================
 echo  N to launch ?
 echo  Z to launch ?
 echo  X to return
 echo  V to renew ip
 echo  C to run a python thing, tbd
 echo  1 to exit
-echo ================================
+echo =================================
 echo Command-Line Interface
-set /p choice="NAK-88 Input>> "
+set /p choice="NAK-DEV2 Input>> "
 if /i "%choice%"=="A" goto LaunchA
 if /i "%choice%"=="B" goto LaunchB
 if /i "%choice%"=="X" goto LaunchX
 if /i "%choice%"=="1" goto end
 if /i "%choice%"=="V" goto launchV
+if /i "%choice%"=="C" goto launchC
 echo Try again.
 pause
 goto menu
@@ -45,6 +46,9 @@ goto menu
 
 :launchV
 ipconfig /renew
+
+:launchC
+python ?.py
 
 :end
 echo Operations completed.
